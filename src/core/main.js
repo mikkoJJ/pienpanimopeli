680ac6e8a3c94475c82b0ded7ec49018d47e8436
elinake <elinakera@gmail.com>
2015-03-01 13:43:14 +0200

    tilaussysteemi toimimaan kirjeiden kautta

--- src/core/main.js
+++ src/core/main.js
@@ -43,7 +43,7 @@
                 this.cursor = this.add.isoSprite(0, 0, 0, 'cursor', 0, this.isoGroup);
             //this.cursor.anchor.setTo(0.5);
 
-            letter = this.add.button(this.width, this.height, 'letter', function() {
+            letter = this.add.button(this.width, this.height, 'letter', function () {
                 Brew.gui.toggleMessages();
             }, this);
             letter.scale.set(0.2, 0.2);
@@ -64,17 +64,17 @@
             scoreText.anchor.set(0.5);
             scoreText.setText("Beer in storage: ");
 
-            this.time.events.loop(Phaser.Timer.SECOND, this.updateCounter, this);
+            this.time.events.loop(Phaser.Timer.SECOND * 5, this.updateCounter, this);
 
             lager = this.beer;
             lager.name = "lager"
-            lager.amount = 50;
+            lager.amount = 1;
 
             sale = this.add.isoSprite(150, 100, 0, 'sell');
             sale.inputEnabled = true;
-            sale.events.onInputDown.add(this.sell, this);
+            //     sale.events.onInputDown.add(this.sell, this);
             sale.beer = this.beer;
-            
+
             kettle = new Kettle(this.game, 250, 100, 0);
 
             Brew.Budget.create();
@@ -82,65 +82,66 @@
             Brew.Budget.update(50);
 
             bottle = this.add.isoSprite(0, 0, 0, 'bottle');
-      //      pullo.body.moves = true;'
+            //      pullo.body.moves = true;'
             bottle.moves = true;
             //ei toimi kummatkaan
             bottle.inputEnabled = true;
             bottle.input.enableDrag(false, true);
             storage.add(bottle);
 
-            Brew.gui.addMessage('Tilaus', '3 tynnyriä tummaa olutta.');
-            Brew.gui.addMessage('Tilaus', '3 tynnyriä lageria.');
-            Brew.gui.addMessage('Sakko', 'Myit liikaa olutta!');
-            Brew.gui.addMessage('Viesti', 'Haluan ostaa olutta! t: Nalle');
+            //tilaukset debuggausta varten
+            var iso = new Order("lageria", 200, "iso tilaus");
+            var pieni = new Order("lageria", 1, "Hemingways");
+            var laiton = new Order("lageria", 1, "Nalle");
+
+            Brew.gui.addMessage('Iso', iso.message(), iso, "Myy", this.sell);
+            Brew.gui.addMessage('Laiton', laiton.message(), laiton, "Myy", this.sell);
+            Brew.gui.addMessage('Pieni', pieni.message(), pieni, "Myy", this.sell);
+            //    Brew.gui.addMessage('Tilaus', '3 tynnyriä lageria.');
+            //    Brew.gui.addMessage('Sakko', 'Myit liikaa olutta!');
+            //    Brew.gui.addMessage('Viesti', 'Haluan ostaa olutta! t: Nalle');
         },
-        
 
         //selling beer
-        sell: function () {
-            if (storage.length <= 0) return;
+        sell: function (order) {
+            if (storage.length < order.amount) return false;
             else {
-                var message = Brew.Budget.update(budget + 10);
-                budget = budget + 10;
-                sale.beer.sell(10);
+                budget = budget + order.price * order.amount;
+                var message = Brew.Budget.update(budget);
+                sale.beer.sell(order.amount);
                 if (budget >= 100) {
                     Brew.gui.alert("Liikevoittosi on ilmiömäinen. " + message);
-              //      sale.inputEnabled = false;
+                    sale.inputEnabled = false;
+                    kettle.inputEnabled = false;
                 }
-                storage.getFirstExists(true).destroy();
+                var j = 0;
+                while (j < order.amount) {
+                    storage.getFirstExists(true).destroy();
+                    j++;
+                }
             }
-
+            if (order.buyer == "Nalle") {
+                Brew.gui.alert("Yksityishenkilölle myyminen on laitonta! Menetit lupasi ja hävisit pelin.");
+            }
         },
-        
-        
+
         /*
-         * sekuntimittari
+         * control orders
          */
         updateCounter: function () {
-            counter++;
+            //   if (i > 5) return;
+            var order = new Order().random();
+            var list = [];
+            list[i++] = Brew.gui.addMessage('Tilaus', order.message(), order, "Myy", this.sell);
 
-            //   console.log(counter);
-            if (counter < 5) {
-                scoreText.setText(counter);
-            } else {
-                output = this.messages.getMessage();
-
-                var list = [];
-                list[i] = this.messages.getMessage();
-                i++;
-                if (list.length > 1) {
-                    //    budget = budget - 500;
-                    //    this.moveProgressBar(100);
-                    //       Brew.gui.alert("Sait sakot! " + sakot, function () {}, this);
-                    //      timer.pause();
-                }
-                //    console.log(list.length);
-                counter = 0;
+            var now = this.time.totalElapsedSeconds();
+            if (now - order.age > 10) {
+                //poista tilaus
             }
         },
 
-        update: function () {            
-            scoreText.setText("Beer in storage: " + this.beer.amount + " litres")
+        update: function () {
+            scoreText.setText("Olutta: " + this.beer.amount + " koria")
 
             this.messages.update();
 
@@ -174,11 +175,11 @@
         }
     };
 
-    
-    
+
+
     /**
      * Kettle IsoSprite that handles the cooking of beer.
-     * 
+     *
      * @class Kettle
      * @augments Phaser.Plugin.Isometric.IsoSprite
      * @param {Phaser.Game} game reference to the currently used game object
@@ -186,22 +187,22 @@
      * @param {Number}      y    iso X position
      * @param {Number}      z    iso X position
      */
-    var Kettle = function(game, x, y, z) {
+    var Kettle = function (game, x, y, z) {
         //call super constructor
         Phaser.Plugin.Isometric.IsoSprite.call(this, game, x, y, z, 'kettle');
-        
+
         this.inputEnabled = true;
         this.events.onInputDown.add(this.cook, this);
-        
+
         game.add.existing(this);
     };
-    
+
     Kettle.prototype = Object.create(Phaser.Plugin.Isometric.IsoSprite.prototype);
     Kettle.prototype.constructor = Kettle;
-    
+
     //cook some beer in the kettle
     Kettle.prototype.cook = function () {
-        if (storage.length >= 15) {   
+        if (storage.length >= 50) {
             budget = budget - 50;
             var message = Brew.Budget.update(budget);
             Brew.gui.alert("Ylitit vuosittaisen tuotantokiintiösi ja sait sakot! " + message);
@@ -210,17 +211,44 @@
             var message = Brew.Budget.update(budget);
             sale.beer.cook(10)
             if (budget <= 0) {
-            //    Brew.gui.alert("hävisit pelin!" + bottle.beer.amount);
+                //    Brew.gui.alert("hävisit pelin!" + bottle.beer.amount);
                 this.inputEnabled = false;
                 Brew.gui.alert(message)
             }
-            var b = this.game.add.isoSprite(0, this.game.rnd.integerInRange(0, 500), 0, 'bottle');
-            b.inputEnabled = true;
-            b.input.enableDrag(false, true);
-            storage.add(b);
+            for (var i = 0; i < 10; i++) {
+                var b = this.game.add.isoSprite(0, this.game.rnd.integerInRange(0, 500), 0, 'bottle');
+                b.inputEnabled = true;
+                b.input.enableDrag(false, true);
+                storage.add(b);
+            }
         }
     };
-    
+
     Brew.Kettle = Kettle;
-    
+
+    var Order = function (type, amount, buyer) {
+        //    this.age = age;
+        this.type = type;
+        this.amount = amount;
+        this.price = 2;
+        this.buyer = buyer;
+    };
+
+    Order.prototype.random = function () {
+        var types = ["lageria", "tummaa olutta", "portteria"];
+        var buyers = ["Kesko", "Hemingways", "Nalle"];
+
+        return new Order(
+            types[Brew.game.rnd.integerInRange(0, types.length - 1)],
+            Brew.game.rnd.integerInRange(1, 10),
+            //     Brew.game.time.totalElapsedSeconds(), 
+            buyers[Brew.game.rnd.integerInRange(0, buyers.length - 1)])
+    };
+
+    Order.prototype.message = function () {
+        return this.amount + " tynnyriä " + this.type + " Tilaaja:" + this.buyer;
+    }
+
+    Brew.Order = Order;
+
 })();
